9d2c5782f2db81b38651b470d4a8bf6824fe310e
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-08 15:34:30 +0300

    Update config.rs

--- src/core/config.rs
+++ src/core/config.rs
@@ -67,7 +67,7 @@ fn default_check() -> String {
 }
 
 fn default_install() -> String {
-    "apk add --no-cache --no-interactive dbus dbus-x11 systemd-libs flatpak discover-backend-flatpak pcmanfm-qt qps qterminal screengrab xdg-desktop-portal-kde openbox labwc breeze-icons onboard postmarketos-ui-plasma-mobile"
+    "apk add --no-cache --no-interactive dbus dbus-x11 dbus-systemd systemd-libs flatpak discover-backend-flatpak pcmanfm-qt qps qterminal screengrab xdg-desktop-portal-kde openbox labwc breeze-icons onboard postmarketos-ui-plasma-mobile"
         .to_string()
 }
 
