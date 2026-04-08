7f820484c2c4a8b5fe9fea9c3b9032ea8a6c2129
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-08 13:57:21 +0300

    Update config.rs

--- src/core/config.rs
+++ src/core/config.rs
@@ -67,7 +67,7 @@ fn default_check() -> String {
 }
 
 fn default_install() -> String {
-    "apk add flatpak discover-backend-flatpak noto-fonts pcmanfm-qt qps qterminal screengrab xdg-desktop-portal-kde openbox xorg-xwayland labwc breeze-icons onboard postmarketos-ui-plasma-mobile"
+    "apk add --no-cache --no-interactive flatpak discover-backend-flatpak noto-fonts pcmanfm-qt qps qterminal screengrab xdg-desktop-portal-kde openbox xorg-xwayland labwc breeze-icons onboard postmarketos-ui-plasma-mobile"
         .to_string()
 }
 
