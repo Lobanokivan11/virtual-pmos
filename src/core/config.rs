700d361b602f16c3c95abc68efa35c1f9b60b592
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-08 18:26:44 +0300

    Update config.rs

--- src/core/config.rs
+++ src/core/config.rs
@@ -72,7 +72,7 @@ fn default_install() -> String {
 }
 
 fn default_launch() -> String {
-    "XDG_RUNTIME_DIR=/tmp WAYLAND_DISPLAY=wayland-0 /usr/lib/plasma-dbus-run-session-if-needed startplasmamobile 2>&1"
+    "mkdir -p /tmp/runtime && chmod 700 /tmp/runtime && XDG_RUNTIME_DIR=/tmp/runtime WAYLAND_DISPLAY=wayland-0 /usr/lib/plasma-dbus-run-session-if-needed startplasmamobile 2>&1"
         .to_string()
 }
 
