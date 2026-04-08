de94a9b148c0ae8f70fb4d0a4e37aa2462be5725
Lobanokivan11 <59411324+Lobanokivan11@users.noreply.github.com>
2026-04-08 14:04:47 +0300

    Update setup.rs

--- src/android/proot/setup.rs
+++ src/android/proot/setup.rs
@@ -326,17 +326,23 @@ fn install_dependencies(options: &SetupOptions) -> StageOutput {
     return Some(thread::spawn(move || {
         let resolv_path = Path::new(ARCH_FS_ROOT).join("etc/resolv.conf");
         fs::write(resolv_path, "nameserver 8.8.8.8\n").expect("Failed to write resolv.conf");
+        let sender_update = mpsc_sender.clone();
         mpsc_sender.send(SetupMessage::Progress("Updating package indices...".to_string())).unwrap_or(());
         ArchProcess {
-            command: "apk update --no-cache --no-interactive".into(),
+            command: "apk update".into(),
             user: None,
-            log: None,
+            log: Some(Arc::new(move |it| {
+                sender_update.send(SetupMessage::Progress(format!("Update: {}", it))).unwrap_or(());
+            })),
         }.run();
+        let sender_ug = mpsc_sender.clone();
         mpsc_sender.send(SetupMessage::Progress("Upgrading system packages...".to_string())).unwrap_or(());
         ArchProcess {
             command: "apk upgrade --no-cache --no-interactive".into(),
             user: None,
-            log: None,
+            log: Some(Arc::new(move |it| {
+                sender_ug.send(SetupMessage::Progress(format!("Upgrade: {}", it))).unwrap_or(());
+            })),
         }.run();
         const MAX_INSTALL_ATTEMPTS: usize = 10;
         // Install dependencies until `check` succeeds.
